97ff31d635788d3206cee2e52de1c801b0df6982
Arnaud TANGUY <arn.tanguy@gmail.com>
2024-01-15 15:47:50 +0100

    [ROS2] Fix INSTALL_PREFIX

--- cmake/brace_top_setup_INSTALL_PREFIX.cmake
+++ cmake/brace_top_setup_INSTALL_PREFIX.cmake
@@ -1,3 +1,2 @@
-set(brace_bottom_setup_INSTALL_PREFIX "${brace_bottom_setup_DIR}/../")
-set(jvrc_description_INSTALL_PREFIX "${jvrc_description_DIR}/../")
-get_filename_component(jvrc_description_INSTALL_PREFIX "${jvrc_description_INSTALL_PREFIX}" REALPATH)get_filename_component(brace_bottom_setup_INSTALL_PREFIX "${brace_bottom_setup_INSTALL_PREFIX}" REALPATH)
+set(brace_top_setup_INSTALL_PREFIX "${brace_top_setup_DIR}/../")
+get_filename_component(brace_top_setup_INSTALL_PREFIX "${brace_top_setup_INSTALL_PREFIX}" REALPATH)

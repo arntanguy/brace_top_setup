a266db9abcac549ae14086bacba591b1e32fbeba
Arnaud TANGUY <arn.tanguy@gmail.com>
2024-01-15 12:44:10 +0100

    Mimic the INSTALL_DESTINATION behavior of catkin

---
+++ cmake/brace_top_setup_INSTALL_PREFIX.cmake
@@ -0,0 +1,3 @@
+set(brace_bottom_setup_INSTALL_PREFIX "${brace_bottom_setup_DIR}/../")
+set(jvrc_description_INSTALL_PREFIX "${jvrc_description_DIR}/../")
+get_filename_component(jvrc_description_INSTALL_PREFIX "${jvrc_description_INSTALL_PREFIX}" REALPATH)get_filename_component(brace_bottom_setup_INSTALL_PREFIX "${brace_bottom_setup_INSTALL_PREFIX}" REALPATH)

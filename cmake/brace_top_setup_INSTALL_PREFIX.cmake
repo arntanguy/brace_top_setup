1288ecf9e7a2cf3207a97063ac7f09962c4c63bb
Arnaud TANGUY <arn.tanguy@gmail.com>
2025-03-11 09:58:40 +0100

    Fix cmake export

--- cmake/brace_top_setup_INSTALL_PREFIX.cmake
+++ cmake/brace_top_setup_INSTALL_PREFIX.cmake
@@ -1,2 +1,2 @@
-set(brace_top_setup_INSTALL_PREFIX "${brace_top_setup_DIR}/../")
+cmake_path(SET brace_top_setup_INSTALL_PREFIX NORMALIZE "${brace_top_setup_DIR}/../")
 get_filename_component(brace_top_setup_INSTALL_PREFIX "${brace_top_setup_INSTALL_PREFIX}" REALPATH)
